1b942252306d732fbb81c82be7d8f343375b142d
Jenni Hay <finalfantasyguru8705@gmail.com>
2020-04-28 02:05:18 -0500

    skeleton api project for pawfriends
    will add logic and testng this week

---
+++ src/main/java/com/pawfriendz/api/pawfriendzapi/PawfriendzApiApplication.java
@@ -0,0 +1,13 @@
+package com.pawfriendz.api.pawfriendzapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawfriendzApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawfriendzApiApplication.class, args);
+    }
+
+}
