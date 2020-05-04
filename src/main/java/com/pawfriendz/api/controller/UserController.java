4e099130e6187deac4252e0853ab692d1ff1da04
Jim Brehm <jim.brehm.55@gmail.com>
2020-05-03 20:11:45 -0500

    PAW-6-LJB-Add Dockerfile and clean up imports

--- src/main/java/com/pawfriendz/api/controller/UserController.java
+++ src/main/java/com/pawfriendz/api/controller/UserController.java
@@ -2,12 +2,10 @@ package com.pawfriendz.api.controller;
 
 import com.pawfriendz.api.model.User;
 import com.pawfriendz.api.service.UserService;
-import com.pawfriendz.api.util.PasswordUtil;
 import com.pawfriendz.dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,32 +15,28 @@ import java.security.NoSuchAlgorithmException;
 
 import static com.pawfriendz.api.util.PasswordUtil.hashPassword;
 
-
 @RestController
 public class UserController {
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
 
-    Logger logger = LoggerFactory.getLogger(UserController.class);
+	Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping("/register")
-    public String sayHi(@Valid @RequestBody UserDTO userDTO
-    )  {
-if(userDTO!=null){
-    User user = null;
-    try {
-        user = new User(userDTO.getUserId(), userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(), hashPassword(userDTO.getPassword()), userDTO.getUsername());
-    } catch (NoSuchAlgorithmException e) {
-        e.printStackTrace();
-    }
-    userService.saveUser(user);
-    logger.info("user " + user.getUserId() + " was saved to the database.");
-}
+	@PostMapping("/register")
+	public String sayHi(@Valid @RequestBody UserDTO userDTO) {
+		if (userDTO != null) {
+			User user = null;
+			try {
+				user = new User(userDTO.getUserId(), userDTO.getFirstName(), userDTO.getLastName(), userDTO.getEmail(),
+						hashPassword(userDTO.getPassword()), userDTO.getUsername());
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+			userService.saveUser(user);
+			logger.info("user " + user.getUserId() + " was saved to the database.");
+		}
 
-
-        return "Thank you for registering, " + (userDTO != null ? userDTO.getUsername() : null) +".";
-    }
-
-
+		return "Thank you for registering, " + (userDTO != null ? userDTO.getUsername() : null) + ".";
+	}
 
 }
--- src/main/java/com/pawfriendz/api/service/UserService.java
+++ src/main/java/com/pawfriendz/api/service/UserService.java
@@ -1,10 +1,7 @@
 package com.pawfriendz.api.service;
 
 import com.pawfriendz.api.model.User;
-import org.springframework.stereotype.Service;
-
 
 public interface UserService {
     public void saveUser(User user);
-
 }
