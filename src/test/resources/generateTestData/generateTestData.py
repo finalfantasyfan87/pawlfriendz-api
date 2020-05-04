6f14ee812a51904893201bd613d2e26fbcf13d1d
Jenni Hay <finalfantasyguru8705@gmail.com>
2020-05-04 05:21:56 -0500

    Update generateTestData.py
    use python faker class, apearantly there are fakers for java, js , etc

--- src/test/resources/generateTestData/generateTestData.py
+++ src/test/resources/generateTestData/generateTestData.py
@@ -1,28 +1,16 @@
-import string
-import random
-import names
+#hnmm cool test datatool
+# https://faker.readthedocs.io/en/
+#also has a java faker class..cool: https://www.baeldung.com/java-faker
 
-
-def passwordGenerator(size=10, chars=string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-def generateRandomString(stringLength=8):
-    lettersAndDigits = string.ascii_letters + string.digits + string.punctuation
-    return ''.join((random.choice(lettersAndDigits) for i in range(stringLength)))
-
-
-def generateRandomEmail(charNum):
-    email = string.ascii_letters + string.digits
-    return ''.join(random.choice(email) for x in range(charNum))
-
-
+from faker import Faker
+fake = Faker()
 data = {
-    "firstName": names.get_first_name(),
-    "lastName": names.get_last_name(),
-    "email": generateRandomEmail(12)+"@gmail.com",
-    "password": passwordGenerator(),
-    "username": generateRandomString(4)
+    "firstName": fake.first_name(),
+    "lastName": fake.last_name(),
+    "email": fake.free_email(),
+    "password": fake.password(length=10),
+    "username": fake.simple_profile()["username"],
+    "phoneNumber": fake.numerify(text='###-###-####')
 }
 
 print(str(data).replace("'", '"'))
