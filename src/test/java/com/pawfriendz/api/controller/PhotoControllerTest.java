5bd3e63abe59be549213cdd024d34c4b646b3aa2
Jim Brehm <jim.brehm.55@gmail.com>
2020-05-24 22:26:15 -0500

    PAW-37-LJB-Change fail assertion o use JUnit 5

--- src/test/java/com/pawfriendz/api/controller/PhotoControllerTest.java
+++ src/test/java/com/pawfriendz/api/controller/PhotoControllerTest.java
@@ -1,7 +1,7 @@
 package com.pawfriendz.api.controller;
 
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
