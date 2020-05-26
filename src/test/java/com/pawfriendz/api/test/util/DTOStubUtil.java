95e99d1865b2f819de15a0b0d52073ef6e52aa6d
Jenni Hay <finalfantasyguru8705@gmail.com>
2020-05-25 19:56:54 -0500

    Update DTOStubUtil.java

--- src/test/java/com/pawfriendz/api/test/util/DTOStubUtil.java
+++ src/test/java/com/pawfriendz/api/test/util/DTOStubUtil.java
@@ -1,17 +1,16 @@
 package com.pawfriendz.api.test.util;
 
+import com.pawfriendz.dto.PhotoDTO;
+import com.pawfriendz.dto.UserDTO;
+import org.springframework.mock.web.MockMultipartFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-import org.springframework.mock.web.MockMultipartFile;
-
-import com.pawfriendz.dto.PhotoDTO;
-import com.pawfriendz.dto.UserDTO;
-
 public class DTOStubUtil {
 	public static UserDTO getUserDTO() {
-		return new UserDTO("123456789", "Uncle", "Iroh", "owner@greendragontea.com", "ALongWayToBaSingSe99!", "iroh1", "555-555-5555", "Zuko");
+		return new UserDTO("123456789", "Uncle", "Iroh", "owner@greendragontea.com", "ALongWayToBaSingSe99!", "iroh1", "555-555-5555", "Zuko",getTestPhoto());
 	}
 	
 	public static PhotoDTO getPhotoDTO() {
